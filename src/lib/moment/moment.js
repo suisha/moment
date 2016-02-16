ed622fe5cd406461f08cce49ee5cb4d66a0fd010
David Mai <david.mai@isocket.com>
2016-02-16 15:00:53 -0800

    add parseZone offset argument

--- src/lib/moment/moment.js
+++ src/lib/moment/moment.js
@@ -4,14 +4,34 @@ import { createInvalid } from '../create/valid';
 import { isMoment } from './constructor';
 import { min, max } from './min-max';
 import { now } from './now';
+import { matchOffset, matchShortOffset } from '../parse/regex';
+import zeroFill from '../utils/zero-fill';
 import momentPrototype from './prototype';
 
 function createUnix (input) {
     return createLocal(input * 1000);
 }
 
-function createInZone () {
-    return createLocal.apply(null, arguments).parseZone();
+function createInZone (input, format, locale, strict, offset) {
+    if (arguments.length === 1) {
+        return local__createLocal.apply(null, arguments).parseZone();
+    }
+
+    if (typeof lastArg === 'number') {
+        var hours =  Math.floor(lastArg / 60);
+        var minutes = lastArg % 60;
+
+        var finalOffset = zeroFill(hours, 2, true) + zeroFill(minutes, 2, false);
+        input += finalOffset;
+    } else if (typeof lastArg === 'string' &&
+              (largeOffset.test(lastArg) || smallOffset.test(lastArg))) {
+        input += lastArg;
+    }
+
+    var inputArg = [input];
+    var otherArgs = Array.prototype.slice.call(arguments, 1, arguments.length - 1);
+
+    return local__createLocal.apply(null, inputArg.concat(otherArgs)).parseZone();
 }
 
 export {
